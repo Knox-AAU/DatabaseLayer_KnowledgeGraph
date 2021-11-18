461d7d5f5000ee098e4273b561dbcf82168aa65c
Christian Bager Bach Houmann <christian@bagerbach.com>
2021-11-18 15:27:37 +0100

    Connection established to Virtuoso

---
+++ RDFApi/RDFApi/Controllers/QueryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using VDS.RDF.Storage;
+
+namespace RDFApi.Controllers
+{
+    public sealed class QueryController : Controller
+    {
+        [HttpGet, Route("/[controller]/status")]
+        public IActionResult Status()
+        {
+            var vm = new VirtuosoManager(
+                Environment.GetEnvironmentVariable("DBA_HOST"), 
+                int.Parse(Environment.GetEnvironmentVariable("DBA_PORT")), 
+                "DB", 
+                Environment.GetEnvironmentVariable("DBA_USERNAME"), 
+                Environment.GetEnvironmentVariable("DBA_PASSWORD"));
+            return Ok(vm.IsReady);
+        }
+    }
+}
