552d6c4222f1ebbe522a27bc9a69afd43d68fa65
Christian Bager Bach Houmann <christian@bagerbach.com>
2021-11-23 11:16:02 +0100

    Slight cleaning and refactoring
    Removed packages and fixed versions

--- RDFApi/RDFApi/Program.cs
+++ RDFApi/RDFApi/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using VDS.RDF.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
 
